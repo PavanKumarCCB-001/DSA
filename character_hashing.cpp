1a4e49883938940a76e8ea3e8e59fa7697827bb8
PavanKumarCCB-001 <gunda.pavan.g@gmail.com>
2025-07-03 12:18:19 +0530

    Program

--- character_hashing.cpp
+++ character_hashing.cpp
@@ -1,16 +1,29 @@
 #include<iostream>
+#include<vector>
 #include<string>
-#include<algorithm>
 using namespace std;
 int main()
 {
     string a;
     cout<<"Enter a String: ";
     getline(cin,a);
-    cout<<"Length of a string is: "<<a.size()<<endl;
-    transform(a.begin(),a.end(),a.begin(),::toupper);
-    cout<<"Uppercase: "<<a<<endl;
-    transform(a.begin(),a.end(),a.begin(),::tolower);
-    cout<<"Lowercase: "<<a;
+
+    // Now let's precompute
+    vector<int>HS(256,0);
+    for(int i=0;i<a.size();i++)
+        HS.at(a.at(i))++;
+
+    cout<<"How many number of characters frequency you want to know: ";
+    int f;
+    cin>>f;
+    vector<char>CV;
+    for(int i=0;i<f;i++)
+    {
+        char e;
+        cin>>e;
+        CV.push_back(e);
+    }
+    for(int i=0;i<f;i++)
+        cout<<"Frequency of "<<CV.at(i)<<": "<<HS.at(CV.at(i))<<endl;
     return 0;
 }
